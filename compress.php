1f316f92417c10cbf8e96e0dce8af48f622e71dc
Thimal Wickremage <thimalmadushanka@gmail.com>
2016-06-14 19:39:09 +0530

    Fixed submit button transition all

--- compress.php
+++ compress.php
@@ -12,7 +12,7 @@ function compress_png($path_to_png_file, $max_quality = 90)
     // '-' makes it use stdout, required to save to $compressed_png_content variable
     // '<' makes it read from the given file path
     // escapeshellarg() makes this safe to use with any path
-    $compressed_png_content = shell_exec("pngquant --quality=$min_quality-$max_quality - < ".escapeshellarg(    $path_to_png_file));
+    $compressed_png_content = shell_exec("pngquant --quality=$min_quality-$max_quality - < ".escapeshellarg($path_to_png_file));
 
     if (!$compressed_png_content) {
         throw new Exception("Conversion to compressed PNG failed. Is pngquant 1.8+ installed on the server?");
