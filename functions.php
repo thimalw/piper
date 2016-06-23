6d229da35f9c7c829356183c5f18c00e75b76d08
Thimal Wickremage <thimalmadushanka@gmail.com>
2016-06-23 21:20:08 +0530

    Cleaned uploads

--- functions.php
+++ functions.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * Pngquant compress
+ */
+function compress_png( $path_to_png_file, $max_quality = 90 ) {
+    if (!file_exists( $path_to_png_file ) ) {
+        throw new Exception( "File does not exist: $path_to_png_file" );
+    }
+
+    // guarantee that quality won't be worse than that.
+    $min_quality = 60;
+
+    // '-' makes it use stdout, required to save to $compressed_png_content variable
+    // '<' makes it read from the given file path
+    // escapeshellarg() makes this safe to use with any path
+    $compressed_png_content = shell_exec( "pngquant --quality=$min_quality-$max_quality - < ".escapeshellarg( $path_to_png_file ) );
+
+    if ( !$compressed_png_content ) {
+        throw new Exception( "Conversion to compressed PNG failed. Is pngquant 1.8+ installed on the server?" );
+    }
+
+    return $compressed_png_content;
+}
+
 function unzip($zip_file_path, $name) {
     $home_folder = dirname(__FILE__)."/uploads/zip/extracted/$name";
     $zip = new ZipArchive;
